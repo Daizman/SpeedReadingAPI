e017330c48e492c4bc40322d0f97b18f2952c393
Daizman <42967651+Daizman@users.noreply.github.com>
2022-07-07 10:35:08 +0500

    Начинаем тестирование Jwt-утилиты, подготовка к тесту авторизации

---
+++ SpeedReading.Tests/WithInMemoryDatabase/Auth/AuthTests.cs
@@ -0,0 +1,15 @@
+﻿using SpeedReading.Persistent;
+using SpeedReading.Tests.WithInMemoryDatabase.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedReading.Tests.WithInMemoryDatabase.Auth
+{
+	public class AuthTests : TestBase
+	{
+
+	}
+}
---
+++ SpeedReading.Tests/WithInMemoryDatabase/Auth/JwtUtilsTests.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using SpeedReading.Application.Common.Models;
+using SpeedReading.Persistent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedReading.Tests.WithInMemoryDatabase.Auth
+{
+	public class JwtUtilsTests
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly IMapper _mapper;
+		private readonly AppSettings _settings;
+
+
+	}
+}
