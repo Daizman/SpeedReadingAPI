23267564d9a695817297cab055614937d8bbc210
Daizman <42967651+Daizman@users.noreply.github.com>
2022-07-09 17:26:36 +0500

    Закомментировал элементы статистики, которые не нужны в БД напрямую

--- SpeedReading.Domain/User/UserDailyStatistic.cs
+++ SpeedReading.Domain/User/UserDailyStatistic.cs
@@ -4,21 +4,21 @@ namespace SpeedReading.Domain.User
 {
 	public class UserDailyStatistic
 	{
-		private static int TaskInPlanCount = 10;
+		// private static int TaskInPlanCount = 10;
 
 		public Guid Id { get; set; }
 		public Guid UserId { get; set; }
 		public DateTime Date { get; set; }
 		public List<TrainingTask> CompletedTasks { get; set; } = new();
-		public List<TrainingTask> AdditionalTasks => PlanedTaskPercent < 100 
+		/*public List<TrainingTask> AdditionalTasks => PlanedTaskPercent < 100 
 			? new() 
-			: CompletedTasks.GetRange(10, CompletedTasksCount - 10);
+			: CompletedTasks.GetRange(10, CompletedTasksCount - 10);*/
 		public int CompletedTasksCount => CompletedTasks.Count;
-		public int PlanedTaskPercent => CompletedTasksCount > TaskInPlanCount 
+		/*public int PlanedTaskPercent => CompletedTasksCount > TaskInPlanCount 
 			? 100 
 			: CompletedTasksCount / TaskInPlanCount * 100; 
 		public int AdditionalTasksCount => CompletedTasksCount > TaskInPlanCount 
 			? CompletedTasksCount - TaskInPlanCount 
-			: 0;
+			: 0;*/
 	}
 }
