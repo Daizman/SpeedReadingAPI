c0a86768877e17ef371ad0e0d6b1a3220ca2ce55
Daizman <42967651+Daizman@users.noreply.github.com>
2022-07-09 17:44:40 +0500

    Наброски TrainingTaskService и Dto к нему

---
+++ SpeedReading.Application/Dtos/Task/TrainingTaskDtos.cs
@@ -0,0 +1,13 @@
+﻿namespace SpeedReading.Application.Dtos.Task
+{
+	public record TrainingTaskDto : IMapWith<Domain.Task.TrainingTask>
+	{
+		public Guid Id { get; init; }
+		public string Title { get; init; }
+		public string Description { get; init; }
+		public string ProgramName { get; init; }
+		public string Skill { get; init; }
+	}
+
+	public record TrainingTaskListDto(IList<TrainingTaskDto> TrainingTasks);
+}
